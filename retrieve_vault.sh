9882cc316ba4031ae57e0afa2e1124deadb244a3
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2020-06-12 21:11:25 +0200

    Updated retrieve_vault, it actually works predictable now

--- retrieve_vault.sh
+++ retrieve_vault.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Retrieve the vault key
 VAULT="$(basename $PWD).gpg"
-VAULTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VAULTDIR="$(dirname `readlink -f $0`)"
+[ -f "$VAULTDIR/$VAULT" ] || exit 1
 cat $VAULTDIR/$VAULT | gpg -qd
