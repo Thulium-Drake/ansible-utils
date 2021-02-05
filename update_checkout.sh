09bd397472adbd49fe67893043f8b8a78b404783
Jeffrey van Pelt <jeff@vanpelt.one>
2021-02-05 11:43:13 +0100

    Made update_checkout.sh more authoritative

--- update_checkout.sh
+++ update_checkout.sh
@@ -9,6 +9,18 @@ GITREPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # As this script is in the scripts folder and we need to run the commands below
 # in the Git root
 cd $GITREPO/..
+
+# Reset entire repo and update
+git reset --hard HEAD
+git clean -f -d -q
+git checkout master
+git fetch origin master
+git reset --hard origin/master
 git pull
-git submodule init
-git submodule update --recursive --remote
+git submodule update
+git submodule update --init --recursive
+git submodule foreach git reset --hard HEAD
+git submodule foreach git clean -f -d -q -x
+git submodule foreach git submodule update --init --recursive
+git submodule foreach git fetch
+git submodule foreach git pull
